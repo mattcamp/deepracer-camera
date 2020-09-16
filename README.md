3f3c8b9e0e846e144b094431dfc0d2f81ef3188d
matt <matt@jigsaw.xyz>
2020-09-16 19:30:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # deepracer-camera
 Displays the camera feed from a physical deepracer car
+
+Note that running this script will log any other sessions out of the web UI so start this feed BEFORE you open the web interface to control the camera (the camera should keep working).
+
+## Installation
+
+1. Create a python3 virtualenv:
+    ```
+    python3 -m venv ./venv
+    source ./venv/bin/activate
+    ```
+   
+2. Install the requirements
+   ```
+   pip install -r requirements.txt
+   ```
+   
+3. Edit dr-cam.py and set the IP of the car and the password
+
+4. run it!
+
+```
+python ./dr-cam.py
+```
+
+Hit Esc to exit.
+
---
+++ dr-cam.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import cv2
+import numpy as np
+import os
+import pprint
+import json
+import urllib3
+urllib3.disable_warnings()
+
+CAR_IP="192.168.0.1"
+PASSWORD = "<password>"
+FULL_SCREEN = False
+
+
+if __name__ == "__main__":
+
+    with requests.Session() as s:
+        URL = "https://%s/" % CAR_IP
+        post_login_url = URL+"login"
+        video_url = URL+"route?topic=/display_mjpeg&width=640&height=480"
+        home_url = URL+"home"
+
+        # Get the CSRF Token
+        response = s.get(URL, verify=False)
+
+        soup = BeautifulSoup(response.text, 'lxml')
+        csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
+        headers = {'X-CSRFToken': csrf_token}
+        # print("CSRF token found: " + str(csrf_token))
+
+        # Login to the DeepRacer web interface with Post
+        payload = {'password': PASSWORD}
+        post = s.post(post_login_url, data=payload, headers=headers, verify=False)
+
+        # Get the video stream
+        video_stream = s.get(video_url, stream=True, verify=False)
+        if video_stream.status_code == 200:
+            print("Video Connected!")
+
+            if FULL_SCREEN:
+            cv2.namedWindow("Deepracer camera stream", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("Deepracer camera stream", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+            bytes = bytes()
+            for chunk in video_stream.iter_content(chunk_size=1024):
+                bytes += chunk
+                a = bytes.find(b'\xff\xd8')  # Marker byte pair
+                b = bytes.find(b'\xff\xd9')  # Trailing byte pair
+                #  If both byte pairs on in the stream then build the jpeg
+                if a != -1 and b != -1:
+                    jpg = bytes[a:b + 2]
+                    bytes = bytes[b + 2:]
+                    i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    window_title = "Deepracer %s camera" % CAR_IP
+                    cv2.imshow('Deepracer camera stream', i)
+
+                    if cv2.waitKey(1) == 27:
+                        exit(0)
+        else:
+            print("Received unexpected status code {}".format(video_stream.status_code))
